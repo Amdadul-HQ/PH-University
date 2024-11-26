fd6939f9174724460a9d393773b5841f7ae3c8a5
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-26 21:17:42 +0600

    add: student interface

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 const app = express();
-const port = 3000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
-
 
 export default app;
