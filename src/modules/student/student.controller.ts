c578958bbcb770f7a19b4938c5b3b67fa17e6e7a
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-27 21:09:19 +0600

    adding trim

--- src/modules/student/student.controller.ts
+++ src/modules/student/student.controller.ts
@@ -14,7 +14,11 @@ const createStudent = async (req: Request, res: Response) => {
       data: result,
     });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+      error: error,
+    });
   }
 };
 
@@ -30,7 +34,11 @@ const getAllStudent = async (req: Request, res: Response) => {
       data: result,
     });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+      error: error,
+    });
   }
 };
 
@@ -47,7 +55,11 @@ const getSingelStudent = async (req: Request, res: Response) => {
       data: result,
     });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({
+        success:false,
+        message:'Something went wrong',
+        error:error
+    })
   }
 };
 
--- src/modules/student/student.model.ts
+++ src/modules/student/student.model.ts
@@ -9,15 +9,18 @@ const studentNameSchema = new Schema<IStudentName>({
     type: String,
     required: [true, 'Student First Name Is Required'],
     maxlength: [10, 'Student First Name Can Not More Than 10 Characters'],
+    trim: true,
   },
   middleName: {
     type: String,
     maxlength: [10, 'Student Middle Name Can Not More Than 10 Characters'],
+    trim: true,
   },
   lastName: {
     type: String,
     required: [true, 'Student Last Name Is Required'],
     maxlength: [10, 'Student Last Name Can Not More Than 10 Characters'],
+    trim: true,
   },
 });
 
@@ -26,29 +29,35 @@ const guardianSchema = new Schema<IGuardian>({
     type: String,
     required: [true, 'Father Name is Required'],
     maxlength: [20, 'Father Name Not more than 20 characters'],
+    trim: true,
   },
   fatherOccupation: {
     type: String,
     required: [true, 'Father Occupation is Required'],
+    trim: true,
   },
   fatherContactNo: {
     type: String,
     required: [true, 'Father Contact Number is Required'],
     maxlength: [11, 'Father Contact Number Can not more then 11 Number'],
+    trim: true,
   },
   motherName: {
     type: String,
     required: [true, 'Mother Name is Required'],
     maxlength: [20, 'Mother Name Not more than 20 characters'],
+    trim: true,
   },
   motherOccupation: {
     type: String,
     required: [true, 'Mother Occupation is Required'],
+    trim: true,
   },
   motherContactNo: {
     type: String,
     required: [true, 'Mother Contact Number is Required'],
     maxlength: [11, 'Mother Contact Number Can not more then 11 Number'],
+    trim: true,
   },
 });
 
@@ -59,19 +68,23 @@ const localGurdianSchema = new Schema<ILocalGuardian>({
     type: String,
     required: [true, 'Local Gurdian Name Is Required'],
     maxlength: [20, 'Local Name Not more than 20 characters'],
+    trim: true,
   },
   occupation: {
     type: String,
     required: [true, 'Local Guridan Occupation Is Required'],
+    trim: true,
   },
   address: {
     type: String,
     required: [true, 'Local Guridan Address Is Rquired'],
+    trim: true,
   },
   contactNo: {
     type: String,
     required: [true, 'Local Guridan Number Is Reqired'],
     maxlength: [11, 'Local Contact Number Can not more then 11 Number'],
+    trim: true,
   },
 });
 
@@ -81,28 +94,34 @@ const studentSchema = new Schema<IStudent>({
   dateOfBirth: {
     type: String,
     required: [true, 'Student Date of Birth Is Required'],
+    trim: true,
   },
   email: {
     type: String,
     required: [true, 'Student Email Is Required'],
+    trim: true,
   },
   contactNo: {
     type: String,
     required: [true, 'Student Contact Number Is Required'],
     maxlength: [11, 'Student Contact Number Can not more then 11 Number'],
+    trim: true,
   },
   emergencyContactNo: {
     type: String,
     required: [true, 'Emergency Number Is Required'],
     maxlength: [11, 'Emergency Contact Number Can not more then 11 Number'],
+    trim: true,
   },
   presentAddress: {
     type: String,
     required: [true, 'Present Address Must be Needed'],
+    trim: true,
   },
   permanentAddress: {
     type: String,
     required: [true, 'Permanent Address Is Required'],
+    trim: true,
   },
   profileImg: {
     type: String,
