467c44b49765d13d85d08b341b00c86bae8adbe8
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-27 20:28:14 +0600

    adding validation

--- src/modules/student/student.model.ts
+++ src/modules/student/student.model.ts
@@ -1,48 +1,103 @@
 import { Schema, model } from 'mongoose';
 import { IStudent } from './student.interface';
 
-// sub sechema
-const commonSchema = {
-  type: String,
-  required: true,
-};
 
 // sub sechema
 
 const studentNameSchema = {
-  firstName: commonSchema,
-  middleName: commonSchema,
-  lastName: commonSchema,
+  firstName: {
+  type: String,
+  required: true,
+},
+  middleName: {
+  type: String,
+  required: true,
+},
+  lastName: {
+  type: String,
+  required: true,
+},
 };
 
 const guardianSchema = {
-  fatherName: commonSchema,
-  fatherOccupation: commonSchema,
-  fatherContactNo: commonSchema,
-  motherName: commonSchema,
-  motherOccupation: commonSchema,
-  motherContactNo: commonSchema,
+  fatherName: {
+  type: String,
+  required: true,
+},
+  fatherOccupation: {
+  type: String,
+  required: true,
+},
+  fatherContactNo: {
+  type: String,
+  required: true,
+},
+  motherName: {
+  type: String,
+  required: true,
+},
+  motherOccupation: {
+  type: String,
+  required: true,
+},
+  motherContactNo: {
+  type: String,
+  required: true,
+},
 };
 
 //sub schema
 
 const localGurdianSchema = {
-  name: commonSchema,
-  occupation: commonSchema,
-  address: commonSchema,
-  contactNo: commonSchema,
+  name: {
+  type: String,
+  required: true,
+},
+  occupation: {
+  type: String,
+  required: true,
+},
+  address: {
+  type: String,
+  required: true,
+},
+  contactNo: {
+  type: String,
+  required: true,
+},
 };
 
 // Schema
 
 const studentSchema = new Schema<IStudent>({
-  dateOfBirth: commonSchema,
-  email: commonSchema,
-  contactNo: commonSchema,
-  emergencyContactNo: commonSchema,
-  presentAddress: commonSchema,
-  permanentAddress: commonSchema,
-  profileImg: commonSchema,
+  dateOfBirth: {
+  type: String,
+  required: true,
+},
+  email: {
+  type: String,
+  required: true,
+},
+  contactNo: {
+  type: String,
+  required: true,
+},
+  emergencyContactNo: {
+  type: String,
+  required: true,
+},
+  presentAddress: {
+  type: String,
+  required: true,
+},
+  permanentAddress: {
+  type: String,
+  required: true,
+},
+  profileImg: {
+  type: String,
+  required: true,
+},
   id: {
     type: String,
   },
