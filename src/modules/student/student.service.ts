8bc1670a80af65ac36e7d492426b5b8e12601450
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-11-30 20:09:11 +0600

    add: zod validation

--- src/modules/student/student.service.ts
+++ src/modules/student/student.service.ts
@@ -3,7 +3,7 @@ import { Student } from './student.model';
 
 // Student register
 const createStudentInToDB = async (student: IStudent) => {
-  const result = await Student.create(student);
+  const result = await Student.create(student); 
   return result;
 };
 
