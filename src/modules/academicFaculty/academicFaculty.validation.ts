5daae874b6a8eb267e16e6772c3660b4e4e46e78
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-12-05 20:55:32 +0600

    add: acdemeic faculty testing

--- src/modules/academicFaculty/academicFaculty.validation.ts
+++ src/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,16 +1,20 @@
 import {z} from 'zod';
 
 const createdAcademicFacultyValidationSchema = z.object({
-    name:z.string({
-        invalid_type_error:'Academic faculty must be string'
-    })
-}) 
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic faculty must be string',
+    }),
+  }),
+});
 
 const updateAcademicFacultyValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic faculty must be string',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic faculty must be string',
+    }),
   }),
-}); 
+});
 
 export const AcademicFacultyValidation = {
     createdAcademicFacultyValidationSchema,
