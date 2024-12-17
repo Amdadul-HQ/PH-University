dc3a0e3018038fd07071b1f86a7d46821a2c6cdf
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-12-17 17:07:26 +0600

    add: token console

--- src/app/middleware/validateRequest.ts
+++ src/app/middleware/validateRequest.ts
@@ -1,16 +1,12 @@
-import { NextFunction, Request, Response } from "express";
 import { AnyZodObject } from "zod";
+import { catchAsync } from "../utils/catchAsync";
 
 const validateRequest = (schema: AnyZodObject) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
+  return catchAsync(async (req, res, next) => {
     // validation
-    try {
       await schema.parseAsync({ body: req.body });
       next();
-    } catch (err) {
-      next(err);
-    }
-  };
+  })
 };
 
 export default validateRequest;
--- src/modules/faculty/faculty.route.ts
+++ src/modules/faculty/faculty.route.ts
@@ -2,11 +2,12 @@ import express from "express";
 import { FacultyController } from "./faculty.controller";
 import validateRequest from "../../app/middleware/validateRequest";
 import { facultyValidation } from "./faculty.validation";
+import auth from "../../app/middleware/auth";
 
 const FacultyRouter = express.Router();
 
 // Get all Faculty
-FacultyRouter.get('/',FacultyController.getAllFaculty);
+FacultyRouter.get('/',auth(),FacultyController.getAllFaculty);
 
 // Get single Faculty
 FacultyRouter.get('/:id',FacultyController.getSingleFaculty);
