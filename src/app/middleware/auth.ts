9a4388d3e600dc9a04ba60ac8e29d49c7ed3e873
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-12-17 17:20:31 +0600

    add; gloabl index.d.ts

--- src/app/middleware/auth.ts
+++ src/app/middleware/auth.ts
@@ -1,15 +1,34 @@
 import httpStatus from "http-status";
 import { AppError } from "../errors/AppError";
 import { catchAsync } from "../utils/catchAsync";
+import jwt, { JwtPayload } from "jsonwebtoken"
+import config from "../config";
+
+
+
 
 const auth = () => {
   return catchAsync(async (req, res, next) => {
     // validation
     const token =req?.headers?.authorization;
 
+    // is token sended
     if(!token){
         throw new AppError(httpStatus.UNAUTHORIZED,'You are not authorized');
     }
+
+    // check token is valid
+    jwt.verify(token,config.jwt_access_secret as string,function(err,decoded){
+        if(err){
+           throw new AppError(
+             httpStatus.UNAUTHORIZED,
+             'You are not authorized',
+           );
+        }
+       
+        req.user = decoded as JwtPayload;
+    })
+
 
     next()
   })
