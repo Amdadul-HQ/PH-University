f2e3168a23f3200b7295c4c1f6f6e49de2f8c12d
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-12-22 18:23:02 +0600

    add: handlevalidation error funtion fix

--- src/app/errors/handleValidationError.ts
+++ src/app/errors/handleValidationError.ts
@@ -3,12 +3,14 @@ import { TErrorSource, TGenericErrorResponse } from "../interface/error";
 
 
 const handleValidationError = (err:mongoose.Error.ValidationError) : TGenericErrorResponse => {
-        const errorSources:TErrorSource = Object.values(err.errors).map((value: mongoose.Error.CastError) =>{
+        const errorSources: TErrorSource = Object.values(err.errors).map(
+          (value: mongoose.Error.ValidatorError | mongoose.Error.CastError) => {
             return {
-                path: value?.path,
-                message:value?.message
-            }
-        })
+              path: value?.path,
+              message: value?.message,
+            };
+          },
+        );
         const statusCode = 400;
         return{
             statusCode,
