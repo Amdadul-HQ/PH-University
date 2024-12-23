d7bd0a67dea36c0ab61248a9396a36e38484b240
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-12-23 17:33:29 +0600

    add: cloudnerly

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -14,4 +14,8 @@ export default {
   jwt_access_expire: process.env.JWT_ACCESS_EXPIRES_IN,
   jwt_refresh_expire: process.env.JWT_REFRESH_EXPIRES_IN,
   rest_pass_ui_link: process.env.RESET_PASS_UI_LINK,
+  cloud_name: process.env.CLOUD_NAME,
+  cloud_api_key: process.env.CLOUD_API_KEY,
+  cloud_api_secret: process.env.CLOUD_API_SECRET,
+  cloud_url: process.env.CLOUDINARY_URL,
 };
