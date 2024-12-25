2b6e3d097e3b5c2e432c3b504a00cc6ea1c9cd00
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-12-25 16:50:29 +0600

    add: student enroller course

--- src/modules/enrollCourse/enrollCourse.service.ts
+++ src/modules/enrollCourse/enrollCourse.service.ts
@@ -1,6 +1,35 @@
+import httpStatus from "http-status";
+import { AppError } from "../../app/errors/AppError";
+import { OfferedCourse } from "../offeredCourse/offeredCourse.model";
 import { IEnrolledCourse } from "./enrollCourse.interface";
+import EnrolledCourse from "./enrollCourse.model";
+import { Student } from "../student/student.model";
 
-const createEnrolledCourseIntoDB = (id:string,payload:IEnrolledCourse) =>{
+const createEnrolledCourseIntoDB = async (id:string,payload:IEnrolledCourse) =>{
+    const {offeredCourse} = payload;
+
+    const isOfferedCourseExists = await OfferedCourse.findById(offeredCourse);
+
+    const student = await Student.findOne({id}).select('id')
+
+    if(!student){
+        throw new AppError(httpStatus.NOT_FOUND,"Student Not Found")
+    }
+
+    if(!isOfferedCourseExists){
+        throw new AppError(httpStatus.NOT_FOUND,"Foofered Course not found")
+    }
+
+    const isStudentAlreadyEnrolled = await EnrolledCourse.findOne({
+        semesterRegistration:isOfferedCourseExists?.semesterRegistration,
+        offeredCourse,
+        student:student?.id
+    })
+
+    if(isStudentAlreadyEnrolled){
+        throw new AppError(httpStatus.CONFLICT,"Student Is Already Enrolled!")
+    };
+    
 
 }
 
