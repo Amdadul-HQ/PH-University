09e155915e751b250bfa6287d8cc6046f9dfe449
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-12-29 18:14:57 +0600

    add: auth to create faculty

--- src/modules/academicFaculty/academicFaculty.route.ts
+++ src/modules/academicFaculty/academicFaculty.route.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import validateRequest from '../../app/middleware/validateRequest';
 import { AcademicFacultyValidation } from './academicFaculty.validation';
 import { AcademicFacultyControllers } from './academicFaculty.controller';
+import auth from '../../app/middleware/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const AcademicFacultyRouter = express.Router();
 
 // careate Academic Faculty
 AcademicFacultyRouter.post(
   '/create-academic-faculty',
+  auth(USER_ROLE.superAdmin),
   validateRequest(
     AcademicFacultyValidation.createdAcademicFacultyValidationSchema,
   ),
